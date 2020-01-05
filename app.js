305185a7a61bdb03828f1d603f449ceb85ad8948
Saentri <ys14076@naver.com>
2020-01-05 11:13:31 +0900

    Completion

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode")
+const saveBtn = document.getElementById("jsSave")
 
 canvas.width = 700;
 canvas.height = 600;
 
-ctx.strokeStyle = "#2c2c2c";
+const INITIAL_COLOR = "#2c2c2c";
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
     painting = false;
@@ -35,6 +42,7 @@ function onMouseMove(event) {
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function handleRangeCharge(event) {
@@ -42,13 +50,41 @@ function handleRangeCharge(event) {
     ctx.lineWidth = size;
 }
 
-function handleModeClick
+function handleModeClick() {
+    if(filling === true) {
+        filling = false;
+        mode.innerText = "채우기";
+    } else {
+        filling = true;
+        mode.innerText = "브러쉬";
+    }
+}
+
+function handleCanvasClick() {
+    if (filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+function handleCM(event) {
+    event.preventDefault();
+}
+
+function handleSaveClick() {
+    const image= canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[EXPORT]";
+    link.click();
+}
 
 if(canvas) {
      canvas.addEventListener("mousemove", onMouseMove);
      canvas.addEventListener("mousedown", startPainting);
      canvas.addEventListener("mouseup", stopPainting);
-     canvas.addEventListener("mouseleave", stopPainting)
+     canvas.addEventListener("mouseleave", stopPainting);
+     canvas.addEventListener("click", handleCanvasClick)
+     canvas.addEventListener("contextmenu", handleCM)
 }
 
 if(colors) {
@@ -57,9 +93,13 @@ if(colors) {
 }
 
 if(range) {
-    range.addEventListener("input", handleRangeCharge)
+    range.addEventListener("input", handleRangeCharge);
 }
 
 if(mode) {
-    mode.addEventListener("clock", handleModeClick)
+    mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick);
 }
